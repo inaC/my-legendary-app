7d04a2883cfb1381e63b9e0fb246d3e07ed2372e
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-03-11 10:24:04 -0300

    Finalize tests

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Learn React. Now!!!! :D
+          Learn React. Now!!!! :D :D XD LOLOL
         </p>
         <a
           className="App-link"
