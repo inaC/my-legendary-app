7eb72a2e222bf0e9e62862dc77d7b95ff4db3c92
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-03-11 09:59:37 -0300

    Updates

---
+++ precache-manifest.7f86e0654f3d7f3a1af1a228c6e3f27f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093c90b87b80fcf0174c38cfdd34654f",
+    "url": "/my-legendary-app/index.html"
+  },
+  {
+    "revision": "450e640d3ce2cc75b2a2",
+    "url": "/my-legendary-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09781e4e8630836475bd",
+    "url": "/my-legendary-app/static/js/2.46d8b6b2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-legendary-app/static/js/2.46d8b6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450e640d3ce2cc75b2a2",
+    "url": "/my-legendary-app/static/js/main.498d8bef.chunk.js"
+  },
+  {
+    "revision": "41e2e00fc8c590ae6bbe",
+    "url": "/my-legendary-app/static/js/runtime-main.3d634b42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-legendary-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
